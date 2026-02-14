96aaf72b4c642c0337b447aa6e2c48686e0c6cc8
PRIYADHARSAN S <priyadharsansofficial@gmail.com>
2026-02-14 11:50:21 +0530

    Update dotenv config path to config/config.env

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import pinsRouter from './routes/pins.js'
 import userRouter from './routes/user.js'
 
 // Load environment variables
-dotenv.config();
+dotenv.config({ path: './config/config.env' });
 
 // Connect to database
 connectDB();
@@ -32,3 +32,4 @@ app.use('/api/v1/user', userRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
